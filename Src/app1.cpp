29dc300f179f8959a188f12f226b6afee1258e9d
mbains <manjinder.bains@gmail.com>
2016-11-12 20:13:26 -0800

    push changes for cpp blink

--- Src/app1.cpp
+++ Src/app1.cpp
@@ -5,6 +5,23 @@
  *      Author: manny
  */
 #include <app1.h>
+class BlinkTest_c 
+{
+public:
+    BlinkTest_c(GPIO_TypeDef * port, uint16_t pin):
+            m_port(port),
+            m_pin(pin)
+    {
+        
+    };
+    void toggle() {
+        
+    }
+private:
+    GPIO_TypeDef * m_port;
+    uint16_t m_pin;
+    
+};
 int app1_main() {
 
 	return 0;
